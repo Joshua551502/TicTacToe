b2327bafd646cd3c2ca8577c1c1d5c317f3b07e5
joshua551502 <mojoshua@sheridancollege.ca>
2023-05-15 19:19:46 -0400

    init

--- tictactoe-main/tictactoegame/script.js
+++ tictactoe-main/tictactoegame/script.js
@@ -45,36 +45,35 @@ function enableBoard() {
 }
 
 function handleClick() {
-    if (this.textContent !== '') {
-        return; // square already selected, do nothing
+
+    if (this.textContent !== '' || winnerFound) {
+        return; // square already selected or a winner is found, do nothing
     }
 
-    disableBoard(); // disable the board before calling computerMove
+    disableBoard(); // disable the board before making the move
 
-    flag = false;
     char = 'X';
 
     if (this.textContent === '') {
         this.textContent = char;
         this.style.color = '#F22901';
         click.play();
-        flag = true;
     }
 
-    if (flag === true) {
-        char = 'O';
+    if (checkWinner('X') === null) {
+        checkFull();
         setTimeout(function () {
-            if (!winnerFound && checkWinner(char) === null && char === 'O') {
+            if (!winnerFound && checkWinner('X') === null) {
                 computerMove();
             }
         }, 1000);
     }
 
-    if (checkWinner('X') === null)
-        checkFull();
-
-
+    if (checkWinner(char) !== null) {
+        disableBoard();
+    }
 }
+
 
 function checkWinner(char) {
     const winningCombinations = [
@@ -100,18 +99,17 @@ function checkWinner(char) {
                 document.getElementById('computer').innerHTML = 'CPU: ' + computerScore++;
                 winner = 'O';
                 lose.play();
-
             }
             winnerFound = true;
             drawLine();
-
-            disableBoard();
-            return;
+            disableBoard(); // Disable the board after a winner is found
+            return winner;
         }
     }
 
     return null;
 }
+
 
 gridItems.forEach(function (item) {
     item.addEventListener('click', handleClick);
@@ -142,52 +140,51 @@ function checkFull() {
 
 function drawLine() {
     const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
-  
+
     const canvas = document.getElementById("line");
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-    for (const combination of winningCombinations) {
-      if (combination.every(cell => gridItems[cell].textContent === winner)) {
-        const startLineId = "item" + (combination[0] + 1);
-        const endLineId = "item" + (combination[combination.length - 1] + 1);
-  
-        const startLine = document.getElementById(startLineId);
-        const endLine = document.getElementById(endLineId);
-  
-        const rect1 = startLine.getBoundingClientRect();
-        const rect2 = endLine.getBoundingClientRect();
-  
-        const startCenterX = rect1.left + rect1.width / 2;
-        const startCenterY = rect1.top + rect1.height / 2;
-        const endCenterX = rect2.left + rect2.width / 2;
-        const endCenterY = rect2.top + rect2.height / 2;
-  
-        const lineWidth = 20;
-        const lineColor = (winner === 'X') ? "red" : "green";
-  
-        ctx.strokeStyle = lineColor;
-        ctx.lineWidth = lineWidth;
-  
-        ctx.beginPath();
-        ctx.moveTo(startCenterX, startCenterY);
-        ctx.lineTo(endCenterX, endCenterY);
-        ctx.stroke();
-      }
-    }
-  
-    if (resetBtn.addEventListener('click', function () {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }));
-  }
-  
 
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const combination = winningCombinations[i];
+        if (combination.every(cell => gridItems[cell].textContent === winner)) {
+            const startLineId = "item" + (combination[0] + 1);
+            const endLineId = "item" + (combination[combination.length - 1] + 1);
+
+            const startLine = document.getElementById(startLineId);
+            const endLine = document.getElementById(endLineId);
+
+            const rect1 = startLine.getBoundingClientRect();
+            const rect2 = endLine.getBoundingClientRect();
+
+            const startCenterX = rect1.left + rect1.width / 2;
+            const startCenterY = rect1.top + rect1.height / 2;
+            const endCenterX = rect2.left + rect2.width / 2;
+            const endCenterY = rect2.top + rect2.height / 2;
+
+            const lineWidth = 15;
+            const lineColor = (winner === 'X') ? "#F22901" : "#08BC6A";
+
+            ctx.strokeStyle = lineColor;
+            ctx.lineWidth = lineWidth;
+
+            ctx.beginPath();
+            ctx.moveTo(startCenterX, startCenterY);
+            ctx.lineTo(endCenterX, endCenterY);
+            ctx.stroke();
+        }
+    }
+
+    if (resetBtn.addEventListener('click', function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }));
+}
